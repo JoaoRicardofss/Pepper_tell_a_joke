8c0f49d334a1691fcdf9095f7eb75f8a33c35af9
João Ricardo Ferreira Santos Silva <38493668+JoaoRicardofss@users.noreply.github.com>
2019-04-18 17:00:49 +0100

    Add files via upload

---
+++ Joke_1.pml
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="Joke_1" format_version="4">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources>
+        <File name="choice_sentences" src="behavior_1/Aldebaran/choice_sentences.xml" />
+        <File name="boobs" src="html/boobs.png" />
+        <File name="byte" src="html/byte.png" />
+        <File name="milk" src="html/milk.jpg" />
+    </Resources>
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ translations/translation_en_US.ts
@@ -0,0 +1,146 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Why are robots shy?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Why are robots shy?</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>I don't know that many jokes, but here is one.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">I don't know that many jokes, but here is one.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Because they have hardware and software but no underware 
+</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Because they have hardware and software but no underware 
+</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Because they have hardware and software but no underware  
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Because they have hardware and software but no underware  
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>why is the robot shy?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">why is the robot shy?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>What did the droid do at lunch time?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">What did the droid do at lunch time?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <source>Had a byte…</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Had a byte…</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Had a byte</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Had a byte</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>How do baby robots drink milk?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">How do baby robots drink milk?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (6)</name>
+        <message>
+            <source>From a robottle</source>
+            <comment>Text</comment>
+            <translation type="obsolete">From a robottle</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>From a ro-bottle</source>
+            <comment>Text</comment>
+            <translation type="unfinished">From a ro-bottle</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (7)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ha Ha Ha Ha</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ha Ha Ha Ha</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ha Ha Ha Ha</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ha Ha Ha Ha</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ha Ha Ha Ha</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ha Ha Ha Ha</translation>
+        </message>
+    </context>
+</TS>
